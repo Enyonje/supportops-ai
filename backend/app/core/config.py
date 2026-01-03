fca46960faaa7d1767cf7f27b4c2678de3fc54a8
Enyonje <nyonjes@gmail.com>
2026-01-03 17:30:39 +0300

    modified:   backend/app/core/config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,8 +1,5 @@
 import os
 
-class Settings:
-    SECRET_KEY: str = os.getenv("JWT_SECRET", "super-secret-change-me")
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
-
-settings = Settings()
+SECRET_KEY = os.getenv("JWT_SECRET", "super-secret-change-me")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24

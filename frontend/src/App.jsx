7de6e90ed8b87f02e6432aae3a93d2a6a8f57311
Enyonje <nyonjes@gmail.com>
2026-01-03 14:25:13 +0300

    modified:   frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,43 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 
-function App() {
+/* Pages */
+import Dashboard from "./pages/Dashboard";
+import IncidentCommandCenter from "./pages/IncidentCommandCenter";
+import Billing from "./pages/Billing";
+import InvestorMode from "./pages/InvestorMode";
+import RevenueForecast from "./pages/RevenueForecast";
+import AutonomousBrain from "./pages/AutonomousBrain";
+import ExecutiveDashboard from "./pages/ExecutiveDashboard";
+import SuccessPage from "./pages/SuccessPage";
+import CancelPage from "./pages/CancelPage";
+
+/* ---------- APP ROOT ---------- */
+
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <Router>
+      <Toaster position="bottom-center" />
+
+      <Routes>
+        {/* Core */}
+        <Route path="/" element={<Dashboard />} />
+
+        {/* Operations */}
+        <Route path="/incidents" element={<IncidentCommandCenter />} />
+        <Route path="/brain" element={<AutonomousBrain />} />
+
+        {/* Business */}
+        <Route path="/billing" element={<Billing />} />
+        <Route path="/revenue-forecast" element={<RevenueForecast />} />
+        <Route path="/investors" element={<InvestorMode />} />
+        <Route path="/executive" element={<ExecutiveDashboard />} />
+
+        {/* Payments */}
+        <Route path="/success" element={<SuccessPage />} />
+        <Route path="/cancel" element={<CancelPage />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;

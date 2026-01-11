60adc72784a5af2deb85b064f0f2de14b4308532
Enyonje <nyonjes@gmail.com>
2026-01-11 12:29:26 +0300

    modified:   src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -7,7 +6,7 @@ import { AuthProvider } from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ Router must wrap everything */}
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>

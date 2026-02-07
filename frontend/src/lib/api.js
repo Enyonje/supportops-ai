1d2ca5327370bf3c010fe7946f950bb6378fa779
Enyonje <nyonjes@gmail.com>
2026-02-07 15:51:55 +0300

    modified:   frontend/src/lib/api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // ✅ dynamic
+  baseURL: import.meta.env.VITE_BACKEND_API_URL, // ✅ dynamic
   withCredentials: true,
   timeout: 15000,
 });
